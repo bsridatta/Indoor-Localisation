2e4b8cbd4b06900aa1bb66b5a0572a01abd2aed8
bsridatta <b.sridatta@gmail.com>
2017-05-13 01:04:13 +0530

    indentations checked

--- TheWifiExperiment/src/trilateration/Circle.java
+++ TheWifiExperiment/src/trilateration/Circle.java
@@ -6,16 +6,16 @@ import java.awt.Graphics;
 public class Circle  {
 
 	public float R;	//corresponding radii
-		public float R1;	//corresponding MODIFIED radii
-			public float X;	//corresponding x-co of center
-				public float Y;	//corresponding y-co of center
+	public float R1;	//corresponding MODIFIED radii
+	public float X;	//corresponding x-co of center
+	public float Y;	//corresponding y-co of center
 
 	public Circle(int x, int y, int r) {
 		// TODO Auto-generated constructor stub
 		X=x;
-			Y=y;
-				R=r;
-					R1=r;
+		Y=y;
+		R=r;
+		R1=r;
 	}
 			
 			//Assuming filled circle intersection 
@@ -26,7 +26,7 @@ public class Circle  {
 	public boolean overlaps(Circle c){//input circle numbers
 		boolean intersects = Math.hypot(X-c.X, Y-c.Y) < (R1 + c.R);
 			//returns if it intersects or no...ii.e true is intersects orelse not
-				//hence true is overlaps 
+			//hence true is overlaps 
 		return intersects;
 	}
 	
@@ -53,9 +53,9 @@ public class Circle  {
 	//it returns 10 percent of the total area of the 2 circles in the view
 	public float delta(Circle c){
 		float delta = 0;
-			float pi=(float) 3.14;
-				float factor =(float) 0.1;//fraction of total area is determined by it
-					delta=(float) (factor*(pi*R*R+pi*c.R*c.R));
+		float pi=(float) 3.14;
+		float factor =(float) 0.1;//fraction of total area is determined by it
+		delta=(float) (factor*(pi*R*R+pi*c.R*c.R));
 		
 		return delta;
 	}
@@ -63,8 +63,8 @@ public class Circle  {
 	public float areaInter(Circle c){
 		//assuming present circle is bigger
 		float Rad =R1;//present circle radius
-			float rad =c.R1;//present circle radius
-				float d =(float) Math.hypot(X - c.X , Y-c.Y) ;//distance btw circle centers
+		float rad =c.R1;//present circle radius
+		float d =(float) Math.hypot(X - c.X , Y-c.Y) ;//distance btw circle centers
 
 		if(Rad < rad){
 		    // swap
@@ -73,8 +73,8 @@ public class Circle  {
 		}
 		
 		float part1 = (float) (rad*rad*Math.acos((d*d + rad*rad - Rad*Rad)/(2*d*rad)));
-			float part2 = (float) (Rad*Rad*Math.acos((d*d + Rad*Rad - rad*rad)/(2*d*Rad)));
-				float part3 = (float) (0.5*Math.sqrt((-d+rad+Rad)*(d+rad-Rad)*(d-rad+Rad)*(d+rad+Rad)));
+		float part2 = (float) (Rad*Rad*Math.acos((d*d + Rad*Rad - rad*rad)/(2*d*Rad)));
+		float part3 = (float) (0.5*Math.sqrt((-d+rad+Rad)*(d+rad-Rad)*(d-rad+Rad)*(d+rad+Rad)));
 		
 		float intersectionArea = part1 + part2 - part3;
 		return intersectionArea;
--- TheWifiExperiment/src/trilateration/WALE.java
+++ TheWifiExperiment/src/trilateration/WALE.java
@@ -19,108 +19,108 @@ public class WALE extends Circle{
 	
 			//*****cyclic  circle increasing function*****
 		
-		public static void CircleIncrease(){
-			//Q is a queue to temporarily save circles for cyclic order
-				//say 3 circles
-						//obviously can do without queue,just to follow the algorithm
+	public static void CircleIncrease(){
+		//Q is a queue to temporarily save circles for cyclic order
+		//say 3 circles
+		//obviously can do without queue,just to follow the algorithm
 			
-			QueueIntf Q= new QueueArray(3);		
-				Q.enqueue(0);//circle 0
-					Q.enqueue(1);//circle 1
-						Q.enqueue(2);//circle 2
+		QueueIntf Q= new QueueArray(3);		
+		Q.enqueue(0);//circle 0
+		Q.enqueue(1);//circle 1
+		Q.enqueue(2);//circle 2
 				
-					int i =0; //counter in while	
+		int i =0; //counter in while	
 			
-			while(!Q.isEmpty()){//infinite loop
+		while(!Q.isEmpty()){//infinite loop
 				
-				i=Q.first();	
-				if(!C[i].overlaps(C[(i+1)%3])|| !C[i].overlaps(C[(i+2)%3])){
-					//if i doesn't intersect with at least 1 of the other 2
+			i=Q.first();	
+			if(!C[i].overlaps(C[(i+1)%3])|| !C[i].overlaps(C[(i+2)%3])){
+			//if i doesn't intersect with at least 1 of the other 2
 					
-					C[i].R1=C[i].R1+beta*C[i].R;
-						System.out.println("increase c["+i+"]  radius to "+C[i].R1 );
-							Q.enqueue(Q.dequeue());//rotating the circles in a cycle							
-				}
-				else{
-					//infinite loop termination point
-break;
-				}
+			C[i].R1=C[i].R1+beta*C[i].R;
+			System.out.println("increase c["+i+"]  radius to "+C[i].R1 );
+			Q.enqueue(Q.dequeue());//rotating the circles in a cycle							
+			}
+	
+			else{
+				//infinite loop termination point
+				break;
 			}
 		}
+	}
 						//*****THE MAIN FUNCTION******	
 		
-		public static void main(String [] args){
+	public static void main(String [] args){
 			
-			C[0]=new Circle(100,100,20);
-				C[1]=new Circle(200,200,40);
-					C[2]=new Circle(150,150,30);
+		C[0]=new Circle(100,100,20);
+		C[1]=new Circle(200,200,40);
+		C[2]=new Circle(150,150,30);
 			
 					//******Distance re-estimation*******
 //overEstimation case1			
-			if(C[0].isInside(C[1])||C[1].isInside(C[2])||C[0].isInside(C[2])){
-				C[0].R1=(float) Math.hypot(C[0].X - C[1].X , C[0].Y-C[1].Y) -C[1].R1;
-					CircleIncrease();
-			}
+		if(C[0].isInside(C[1])||C[1].isInside(C[2])||C[0].isInside(C[2])){
+		C[0].R1=(float) Math.hypot(C[0].X - C[1].X , C[0].Y-C[1].Y) -C[1].R1;
+		CircleIncrease();
+		}
 //under estimation
-			else if(!C[0].overlaps(C[1])||!C[0].overlaps(C[2])||!C[1].overlaps(C[2])){
-					CircleIncrease();
-			}
+		else if(!C[0].overlaps(C[1])||!C[0].overlaps(C[2])||!C[1].overlaps(C[2])){
+			CircleIncrease();
+		}
 
 //overEstimation case2					
-			else{
-				if((C[0].areaInter(C[1])>C[0].delta(C[1]) || C[0].areaInter(C[2])>C[0].delta(C[2]) || C[1].areaInter(C[2])>C[1].delta(C[2]) ) && !(!C[0].overlaps(C[1])||!C[0].overlaps(C[2])||!C[1].overlaps(C[2])) ){
-				// this and underestimation is false then overestimation 2 is true
-					//as its in else case its shouldv'e already been covered
-						//but as the radius is dynamically reduced it needs to be checked again
+		else{
+			if((C[0].areaInter(C[1])>C[0].delta(C[1]) || C[0].areaInter(C[2])>C[0].delta(C[2]) || C[1].areaInter(C[2])>C[1].delta(C[2]) ) && !(!C[0].overlaps(C[1])||!C[0].overlaps(C[2])||!C[1].overlaps(C[2])) ){
+			// this and underestimation is false then overestimation 2 is true
+			//as its in else case its shouldv'e already been covered
+			//but as the radius is dynamically reduced it needs to be checked again
 					
-					while((C[0].areaInter(C[1])>C[0].delta(C[1]) || C[0].areaInter(C[2])>C[0].delta(C[2]) || C[1].areaInter(C[2])>C[1].delta(C[2]) ) && !(!C[0].overlaps(C[1])||!C[0].overlaps(C[2])||!C[1].overlaps(C[2])) ){
+				while((C[0].areaInter(C[1])>C[0].delta(C[1]) || C[0].areaInter(C[2])>C[0].delta(C[2]) || C[1].areaInter(C[2])>C[1].delta(C[2]) ) && !(!C[0].overlaps(C[1])||!C[0].overlaps(C[2])||!C[1].overlaps(C[2])) ){
 					
-						C[0].R1=C[0].R1 - beta* C[0].R;
-							System.out.println("increase c[0]");
-						C[1].R1=C[1].R1 - beta* C[1].R;
-							System.out.println("increase c[1]");
-					}
+					C[0].R1=C[0].R1 - beta* C[0].R;
+					System.out.println("increase c[0]");
+					C[1].R1=C[1].R1 - beta* C[1].R;
+					System.out.println("increase c[1]");
 				}
 			}
+		}
 			
 				//*********Location estimation**********
-	System.out.println("done dce");
-			float  W[] = new float[100] ;
+		System.out.println("done dce");
+		float  W[] = new float[100] ;
 			
-			for(int i=0;i<3;i++){
+		for(int i=0;i<3;i++){
 				
-				int t=0;
-				
-				for(int k=0;k<C[i].R1;k++){
-					W[k]=(float) ((1/C[i].R)*(Math.exp(k/C[i].R)));
-					System.out.println(W[k]);
-
+			int t=0;
+			for(int k=0;k<C[i].R1;k++){
+				W[k]=(float) ((1/C[i].R)*(Math.exp(k/C[i].R)));
+				System.out.println(W[k]);
 				//for every point "j" on the varying circumference of radius "k"
-					  float x_co=(float) (C[i].X+k*Math.cos(t*3.14/180));
-					  	float y_co=(float) (C[i].Y+k*Math.sin(t*3.14/180));
-					  		point j= new point();
-					  			j.x=x_co;
-					  				j.y=y_co;
+				float x_co=(float) (C[i].X+k*Math.cos(t*3.14/180));
+				float y_co=(float) (C[i].Y+k*Math.sin(t*3.14/180));
+				point j= new point();
+				j.x=x_co;
+				j.y=y_co;
 				// point on the varying k
 				//check if it lies inside other circles
-					  if(C[(i+1)%3].hasPoint(x_co, y_co)||C[(i+2)%3].hasPoint(x_co, y_co)){
-						  if(j.isTrilaterable()){// checking if the point is trilaterable
-							  j.weight=j.weight+W[k];
-						  }
-						  else{
-							  j.weight=Math.abs(j.weight-W[k]);
-						  }
-					  }
-					  else{
-						  j.weight=W[k];
-					  }
+				if(C[(i+1)%3].hasPoint(x_co, y_co)||C[(i+2)%3].hasPoint(x_co, y_co)){
+					if(j.isTrilaterable()){// checking if the point is trilaterable
+						j.weight=j.weight+W[k];
+					}
+					else{
+						j.weight=Math.abs(j.weight-W[k]);
+					}
 				}
-				for(float s:W){
-					System.out.println(s);
+				else{
+					j.weight=W[k];
 				}
-				
 			}
-		}		
+			
+			for(float s:W){
+				System.out.println(s);
+			}
+				
+		}
+	}		
 		
 }	
 
